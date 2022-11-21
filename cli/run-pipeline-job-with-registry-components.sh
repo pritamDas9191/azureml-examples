07f9b59e39716e985315ee1aa0eb100bbea2b87d
Ubuntu <fvm@azureml.hdto2glhnacenmzoxwwt5zmpza.phxx.internal.cloudapp.net>
2022-11-21 19:53:06 +0000

    Create workflows to run pipeline job using registry components

--- cli/run-pipeline-job-with-registry-components.sh
+++ cli/run-pipeline-job-with-registry-components.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
 
 cp $pipeline.yml ${pipeline}-registry-components.yml
-sed -i -r "s/component:.+\/(.+).yml|azureml:(.+)@latest/azureml:\/\/registries\/$REGISTRY_NAME\/components\/\1\2_$folder\/versions\/1/g" ${pipeline}-registry-components.yml
+sed -i -r "s/component:.+\/(.+).yml|azureml:(.+)@latest/component: azureml:\/\/registries\/$REGISTRY_NAME\/components\/\1\2_$folder\/versions\/1/g" ${pipeline}-registry-components.yml
 cat ${pipeline}-registry-components.yml
 
 bash $SCRIPT_DIR/run-job.sh ${pipeline}-registry-components.yml
